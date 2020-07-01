59961ba39ea655760dfe6a8d001f20a36458d5c5
Monteiro <clayton.monteiro@gmail.com>
2020-07-01 13:22:19 -0300

    Listagem
    Listagem de ativos

--- fundamentus.js
+++ fundamentus.js
@@ -39,13 +39,56 @@ class FundamentusService {
               }
             });
           });
-          return res.status(200).json(ativo)
+          return res.status(200).json(ativo);
         }
       });
-      
   }
 
+  async listaAtivos(req, res, next) {
+    await axios
+      .request({
+        method: "GET",
+        url: `https://www.fundamentus.com.br/detalhes.php`,
+        responseType: "arraybuffer",
+        responseEncoding: "binary",
+      })
+      .then(function (response) {
+        if (response.status === 200) {
+          let html = iso88592.decode(response.data.toString("binary"));
 
+          const $ = cheerio.load(html, { decodeEntities: true });
+
+          const tables = $(".resultado");
+
+          const ativos = [];
+          const table_ativo = $(tables[0]).find("tr");
+          table_ativo.map((i, data) => {
+            let item = $(data).find("td");
+
+            const ativo = {};
+            item.map((i, span) => {
+              let x = $(span).text();
+
+              switch (i) {
+                case 0:
+                  ativo["papel"] = x;
+                  break;
+                case 1:
+                  ativo["empresa"] = x;
+                  break;
+                case 2:
+                  ativo["razao_social"] = x;
+                  break;
+              }
+            });
+
+            ativos.push(ativo);
+          });
+
+          return res.status(200).json(ativos);
+        }
+      });
+  }
 }
 
 module.exports = new FundamentusService();
--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const FundamentusService = require('./fundamentus')
 
 function routes(App) {
     App.get('/api/fundamentus/ativo', FundamentusService.dadosAtivo)
+    App.get('/api/fundamentus/listaativo', FundamentusService.listaAtivos)
 }
+
 
 exports.routes = routes
